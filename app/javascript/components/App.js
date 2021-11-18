6621177f043f393c16d4fd6e03af127dfeef20fd
Chawwh <chachablueftw@gmail.com>
2021-11-18 12:02:23 -0700

    adds footer element with links

--- app/javascript/components/App.js
+++ app/javascript/components/App.js
@@ -1,30 +1,25 @@
-import React, { Component } from "react"
-import Header from "./components/Header"
-import {
-  BrowserRouter as Router,
-  Route,
-  Switch
-} from 'react-router-dom'
+import React, { Component } from "react";
+import Header from "./components/Header";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 
-
-class App extends Component{
-  render () {
+class App extends Component {
+  render() {
     const {
       logged_in,
       current_user,
       new_user_route,
       sign_in_route,
-      sign_out_route
-    } = this.props
+      sign_out_route,
+    } = this.props;
     return (
       <>
-      <Router>
-      <Header {...this.props}/>
-      </Router>
-      <h1>Welcome to FlixList</h1>
+        <Router>
+          <Header {...this.props} />
+        </Router>
+        <h1>Welcome to FlixList</h1>
       </>
     );
   }
 }
 
-export default App
+export default App;
