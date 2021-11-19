562c4a9de0002b098779791df2899252215aab0e
Chawwh <chachablueftw@gmail.com>
2021-11-19 16:09:02 -0700

    adds a mock movies file and cards for bucket

---
+++ app/javascript/components/mockMovies.js
@@ -0,0 +1,47 @@
+let movies = [
+    {  
+        id: 1,
+        title: "movie 1",
+        genre: "fantasy",
+        release_date: "2012",
+        runtime: "120 mins",
+        movie: true,
+        tv_show: false,
+        seasons: 0,
+        img: "https://i.ytimg.com/vi/tiZ8u6PNTK8/maxresdefault.jpg"  
+    },
+    {  
+        id: 2,
+        title: "movie 2",
+        genre: "horror",
+        release_date: "2013",
+        runtime: "150 mins",
+        movie: true,
+        tv_show: false,
+        seasons: 0,
+        img: "https://i.ytimg.com/vi/tiZ8u6PNTK8/maxresdefault.jpg"  
+    },
+    {  
+        id: 3,
+        title: "movie 3",
+        genre: "comedy",
+        release_date: "2014",
+        runtime: "69 mins",
+        movie: true,
+        tv_show: false,
+        seasons: 0,
+        img: "https://i.ytimg.com/vi/tiZ8u6PNTK8/maxresdefault.jpg"  
+    },
+    {  
+        id: 4,
+        title: "movie 4",
+        genre: "drama",
+        release_date: "2015",
+        runtime: "90 mins",
+        movie: true,
+        tv_show: false,
+        seasons: 0,
+        img: "https://i.ytimg.com/vi/tiZ8u6PNTK8/maxresdefault.jpg"  
+    }
+]
+export default movies
--- app/javascript/components/pages/Bucket.js
+++ app/javascript/components/pages/Bucket.js
@@ -1,10 +1,66 @@
 import React, { Component } from "react";
+import movies from "../mockMovies";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
+import {
+  Card,
+  CardImg,
+  CardBody,
+  CardGroup,
+  CardSubtitle,
+  CardText,
+  CardTitle,
+  Button,
+} from "reactstrap";
+import { faStar } from "@fortawesome/free-solid-svg-icons";
 
 class Bucket extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      movies: movies,
+    };
+  }
   render() {
-    return <>
-    <h1>This is the entertainment bucket page</h1>
-    </>;
+    return (
+      <>
+        <div className="body-container">
+          <h1>Your Personalized Entertainment Bucket</h1>
+          {this.state.movies &&
+            this.state.movies.map((movie) => {
+              return (
+                <>
+                    <Card
+                      key={movie.id}
+                        color="dark"
+                        inverse
+                    >
+                      <CardImg
+                        alt="Card image cap"
+                        src={movie.img}
+                        top
+                        width="100%"
+                      />
+                      <CardBody>
+                        <CardTitle tag="h5">{movie.title}</CardTitle>
+                        <FontAwesomeIcon 
+                        icon={faStar}
+                        style={{color: "#FFE401"}}
+                        />
+                        
+                        <CardSubtitle className="mb-2 text-muted" tag="h6">
+                          Genre: {movie.genre}
+                          <br />
+                          Runtime: {movie.runtime}
+                        </CardSubtitle>
+                        <Button>Add to Favorites</Button>
+                      </CardBody>
+                    </Card>
+                </>
+              );
+            })}
+        </div>
+      </>
+    );
   }
 }
 export default Bucket;
