7ddc8ad72c47a4327196685b6aeb68170c7dab79
etsegay <etsegay@miu.edu>
2021-11-18 19:04:47 -0500

    header buttons, center home

--- app/javascript/components/components/Header.js
+++ app/javascript/components/components/Header.js
@@ -1,31 +1,43 @@
-import React, { Component } from "react"
-import pic from "../assets/applogo.png"
-import { NavLink } from 'react-router-dom'
+import React, { Component } from "react";
+import pic from "../assets/applogo.png";
+import { NavLink } from "react-router-dom";
+import { Button } from "reactstrap";
 
-class Header extends Component{
-render(){
+class Header extends Component {
+  render() {
     const {
-        logged_in,
-        current_user,
-        new_user_route,
-        sign_in_route,
-        sign_out_route
-      } = this.props
-    return(
-        <header>
-            <div className="nav-links">
-            <NavLink to="/">
-                <img id="logo-img" src={pic} alt="flixlist logo"></img>
-            </NavLink>
-            {!logged_in &&
+      logged_in,
+      current_user,
+      new_user_route,
+      sign_in_route,
+      sign_out_route,
+    } = this.props;
+    return (
+      <header>
+        <div className="header-container">
+          <NavLink to="/">
+            <img id="logo-img" src={pic} alt="flixlist logo"></img>
+          </NavLink>
+
+          {!logged_in && (
             <>
-                <a  href={sign_in_route}>Login</a>
-                <a  href={new_user_route}>Sign Up</a> 
+                <div className="nav-bar">
+              <Button color="primary">
+                <a href={sign_in_route}>
+                  <span>Login</span>
+                </a>
+              </Button>
+              <Button color="primary">
+                <a href={new_user_route}>
+                  <span>Sign Up</span>
+                </a>
+              </Button>
+              </div>
             </>
-            }
-            </div>
-        </header>
-    )
+          )}
+        </div>
+      </header>
+    );
+  }
 }
-}
-export default Header
+export default Header;
