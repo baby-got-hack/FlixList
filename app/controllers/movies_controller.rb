022799d9dbd5691a25d5608059816963cc7580f0
Chawwh <chachablueftw@gmail.com>
2021-11-22 14:16:53 -0700

    starting spec for movie controller

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,2 +1,8 @@
 class MoviesController < ApplicationController
+
+    def index
+         movie = Movie.all
+         render json: movie
+    end
+
 end
--- spec/requests/movies_spec.rb
+++ spec/requests/movies_spec.rb
@@ -1,7 +1,45 @@
 require 'rails_helper'
 
 RSpec.describe "Movies", type: :request do
+  let(:movie){ nil }
+  let(:genre){ nil }
+  let(:movie_params) do
+    { 
+      movie: movie,
+      genre: genre
+    }
+  end
+
+  let(:json){ JSON.parse(response.body)}
+
   describe "GET /index" do
-    pending "add some examples (or delete) #{__FILE__}"
+    before do
+      get '/movies', params: movie_params
+    end
+
+    it 'comes back as status 200' do
+      expect(response).to have_http_status(:ok)
+    end
+
+    context 'when genre is set' do
+      let(:genre){'drama'}
+      let!(:drama_one){ 
+        Movie.create  title: 'Super drama movie', genre: 'drama'
+      }
+      let!(:drama_two){ 
+        Movie.create  title: 'Other super drama movie', genre: 'drama'
+      }
+      let!(:comedy_one){ 
+        Movie.create  title: 'Super funny movie', genre: 'comedy' 
+      }
+
+      it 'returns movies filtered by drama' do
+        binding.pry
+        expect(json[0].id).to eq drama_two.id
+        expect(json[1].id).to eq drama_one.id
+        expect(json.length).to eq 2
+      end
+    end
+
   end
 end
