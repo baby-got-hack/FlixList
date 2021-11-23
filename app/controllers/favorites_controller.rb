d0fd43e3b6aa53b81866cfb3c3c88fd126a4adfb
pdisi <pdisidoro@yahoo.com>
2021-11-23 09:07:34 -1000

    starting favorites controller

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -1,2 +1,6 @@
 class FavoritesController < ApplicationController
+    def create
+        favorite = Favorite.create(user_id: params[:user_id], movie_id: params[:movie_id])
+        render json: favorite
+    end
 end
