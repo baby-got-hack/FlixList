0db1c94b4a95a1bd9eb1b1866d9a1777931cbdd3
Chawwh <chachablueftw@gmail.com>
2021-11-24 12:47:54 -0700

    formatted favorites page

--- app/javascript/components/pages/Favorites.js
+++ app/javascript/components/pages/Favorites.js
@@ -3,38 +3,29 @@ import React, { Component } from "react";
 class Favorites extends Component {
   render() {
     return (
-      <>
-        <div className="body-container">
-          <>
-          <h1>This is the favorites page</h1>
-          <CardGroup>
-                      <Card
-                        key={movie.id}
-                        color="dark"
-                        inverse
-                        className="bucket-card"
-                      >
-                        <CardImg
-                          alt="Card image cap"
-                          src={movie.img}
-                          top
-                          width="100%"
-                          id="card-img"
-                        />
-                        <CardBody>
-                          <CardTitle tag="h5">{movie.title}</CardTitle>
+      <div className="body-container">
+        <h1>This is the favorites page</h1>
+        <CardGroup>
+          <Card key={movie.id} color="dark" inverse className="bucket-card">
+            <CardImg
+              alt="Card image cap"
+              src={movie.img}
+              top
+              width="100%"
+              id="card-img"
+            />
+            <CardBody>
+              <CardTitle tag="h5">{movie.title}</CardTitle>
 
-                          <CardSubtitle className="mb-2 text-muted" tag="h6">
-                            Genre: {movie.genre}
-                            <br />
-                            Runtime: {movie.runtime}
-                          </CardSubtitle>
-                        </CardBody>
-                      </Card>
-                    </CardGroup>
-          </>
-        </div>
-      </>
+              <CardSubtitle className="mb-2 text-muted" tag="h6">
+                Genre: {movie.genre}
+                <br />
+                Runtime: {movie.runtime}
+              </CardSubtitle>
+            </CardBody>
+          </Card>
+        </CardGroup>
+      </div>
     );
   }
 }
