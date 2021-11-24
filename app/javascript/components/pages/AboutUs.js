c32928c9e6be9f1c6720f35fa1029d27f84ff177
Lilmcg <lilmcg@gmail.com>
2021-11-24 13:13:06 -0800

    formatting aboutus and scss

--- app/javascript/components/pages/AboutUs.js
+++ app/javascript/components/pages/AboutUs.js
@@ -5,113 +5,104 @@ import Sahtra from "../assets/Sahtra.jpg";
 import Elias4 from "../assets/Elias4.png";
 import LinkedIn from "../assets/icons8-linkedin-2-48.png";
 import {
-    Card,
-    CardGroup,
-    CardImg,
-    CardBody,
-    CardSubtitle,
-    CardTitle,
-    Button,
-    CardText,
-  } from "reactstrap";
+  Card,
+  CardGroup,
+  CardImg,
+  CardBody,
+  CardSubtitle,
+  CardTitle,
+  Button,
+  CardText,
+} from "reactstrap";
 
 class AboutUs extends Component {
   render() {
     return (
       <>
-      <div className="body-container">
-        <div className="aboutus-upper">
-          <h1>Meet the FlixList Creators!</h1>
-          <div className="aboutus-body"></div>
-    </div>
-        <>
-        <CardGroup>
-          <Card color="dark" className="aboutus-card">
-            <CardImg
-              alt="Card image cap"
-              src={Philip}
-              top
-              width="25%"
-            />
-            <CardBody>
-              <CardTitle tag="h5">PHILIP DISIDORO</CardTitle>
-              <CardSubtitle className="mb-2 text-muted" tag="h6">
-                Product Manager
-              </CardSubtitle>
-              <CardText>
-                <br />
-                "There are 360 degrees, so why only stick to one? Zaha Hadid"
-                <br />
-                ~Zaha Hadid
-              </CardText>
-              <a href="https://www.linkedin.com/in/pdisidoro/"><img src={LinkedIn} className="linkedin-btn"/></a>
-            </CardBody>
-          </Card>
-          <Card color="dark" className="aboutus-card">
-            <CardImg
-              alt="Card image cap"
-              src={Lillian3}
-              top
-              width="25%"
-            />
-            <CardBody>
-              <CardTitle tag="h5">LILLIAN McGILLIVRAY</CardTitle>
-              <CardSubtitle className="mb-2 text-muted" tag="h6">
-                Project Manager
-              </CardSubtitle>
-              <CardText>
-                <br />
-                "If you think it's expensive to hire a professional to do the job, just wait until you hire an amateur"
-                <br />
-                ~83oranges
-              </CardText>
-              <a href="https://www.linkedin.com/in/lillian-mcgillivray-8118315/"><img src={LinkedIn} className="linkedin-btn"/></a>
-            </CardBody>
-          </Card>
-          <Card color="dark" className="aboutus-card">
-            <CardImg
-              alt="Card image cap"
-              src={Sahtra}
-              top
-              width="25%"
-            />
-            <CardBody>
-              <CardTitle tag="h5">SAHTRA 'CHAWWH'</CardTitle>
-              <CardSubtitle className="mb-2 text-muted" tag="h6">
-                Tech Lead
-              </CardSubtitle>
-              <CardText>
-                <br />
-                "Life is a tornado and I'm the cow spinning around for cinematic effect"
-                <br />
-                ~@69possums420
-              </CardText>
-              <a href="https://www.linkedin.com/in/Chawwh/"><img src={LinkedIn} className="linkedin-btn"/></a>
-            </CardBody>
-          </Card>
-          <Card color="dark" className="aboutus-card">
-            <CardImg
-              alt="Card image cap"
-              src={Elias4}
-              top
-              width="25%"
-            />
-            <CardBody>
-              <CardTitle tag="h5">ELIAS TSEGAY</CardTitle>
-              <CardSubtitle className="mb-2 text-muted" tag="h6">
-                Design Lead
-              </CardSubtitle>
-              <CardText>
-              <br />
-              "One man’s crappy software is another man’s full time job"
-              <br />
-              ~Jessica Gaston
-              </CardText>
-              <a href="https://www.linkedin.com/in/elias-zerai-b1693a134//Elias"><img src={LinkedIn} className="linkedin-btn"/></a>
-            </CardBody>
-          </Card>
-        </CardGroup>
-        </>
+        <div className="body-container">
+          <div className="aboutus-upper">
+            <h1>Meet the FlixList Creators!</h1>
+            <div className="aboutus-body"></div>
+          </div>
+          <>
+            <CardGroup>
+              <Card color="dark" className="aboutus-card">
+                <CardImg alt="Card image cap" src={Philip} top width="25%" />
+                <CardBody>
+                  <CardTitle tag="h5">PHILIP DISIDORO</CardTitle>
+                  <CardSubtitle className="mb-2 text-muted" tag="h6">
+                    Product Manager
+                  </CardSubtitle>
+                  <CardText>
+                    <br />
+                    "There are 360 degrees, so why only stick to one? Zaha
+                    Hadid"
+                    <br />
+                    ~Zaha Hadid
+                  </CardText>
+                  <a href="https://www.linkedin.com/in/pdisidoro/">
+                    <img src={LinkedIn} className="linkedin-btn" />
+                  </a>
+                </CardBody>
+              </Card>
+              <Card color="dark" className="aboutus-card">
+                <CardImg alt="Card image cap" src={Lillian3} top width="25%" />
+                <CardBody>
+                  <CardTitle tag="h5">LILLIAN McGILLIVRAY</CardTitle>
+                  <CardSubtitle className="mb-2 text-muted" tag="h6">
+                    Project Manager
+                  </CardSubtitle>
+                  <CardText>
+                    <br />
+                    "If you think it's expensive to hire a professional to do
+                    the job, just wait until you hire an amateur"
+                    <br />
+                    ~83oranges
+                  </CardText>
+                  <a href="https://www.linkedin.com/in/lillian-mcgillivray-8118315/">
+                    <img src={LinkedIn} className="linkedin-btn" />
+                  </a>
+                </CardBody>
+              </Card>
+              <Card color="dark" className="aboutus-card">
+                <CardImg alt="Card image cap" src={Sahtra} top width="25%" />
+                <CardBody>
+                  <CardTitle tag="h5">SAHTRA 'CHAWWH'</CardTitle>
+                  <CardSubtitle className="mb-2 text-muted" tag="h6">
+                    Tech Lead
+                  </CardSubtitle>
+                  <CardText>
+                    <br />
+                    "Life is a tornado and I'm the cow spinning around for
+                    cinematic effect"
+                    <br />
+                    ~@69possums420
+                  </CardText>
+                  <a href="https://www.linkedin.com/in/Chawwh/">
+                    <img src={LinkedIn} className="linkedin-btn" />
+                  </a>
+                </CardBody>
+              </Card>
+              <Card color="dark" className="aboutus-card">
+                <CardImg alt="Card image cap" src={Elias4} top width="25%" />
+                <CardBody>
+                  <CardTitle tag="h5">ELIAS TSEGAY</CardTitle>
+                  <CardSubtitle className="mb-2 text-muted" tag="h6">
+                    Design Lead
+                  </CardSubtitle>
+                  <CardText>
+                    <br />
+                    "One man’s crappy software is another man’s full time job"
+                    <br />
+                    ~Jessica Gaston
+                  </CardText>
+                  <a href="https://www.linkedin.com/in/elias-zerai-b1693a134//Elias">
+                    <img src={LinkedIn} className="linkedin-btn" />
+                  </a>
+                </CardBody>
+              </Card>
+            </CardGroup>
+          </>
         </div>
       </>
     );
