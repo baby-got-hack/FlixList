0be0c20e7e71ee29405473badf7dc40ec0c748e5
Chawwh <chachablueftw@gmail.com>
2021-12-01 22:55:04 -0700

    moved devise routes higher in routes file

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,11 @@
 Rails.application.routes.draw do
   resources :movies
+  devise_for :users
   resources :favorites do
     collection do
     get 'movie_data'
     end
   end
-  devise_for :users
   get '*path', to: 'home#index', constraints: ->(request){ request.format.html? }
   root 'home#index'
   get '/movies', to: 'movies#index'
